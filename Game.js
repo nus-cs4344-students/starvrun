f656b71e13f21bae92ed2187b62dfb1cc4c1e417
yanxian <duyanxian1990@hotmail.com>
2015-03-09 13:23:32 +0800

    Game.js : added render() skeleton code

---
+++ Game.js
@@ -0,0 +1,15 @@
+function Game(){
+    
+    
+
+    /*
+     * private method: render
+     *
+     * Draw the play area.  Called periodically at a rate
+     * equals to the frame rate.
+     */
+    var render = function(){
+        
+    };
+       
+}
