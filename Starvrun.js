3e6148e115272896974ba1ad29a622db8b0ba50a
Edrick Rudy Putra <edrick@pigeonlab.com>
2015-03-09 14:36:18 +0800

    --add: add constants to Starvrun,js

---
+++ Starvrun.js
@@ -0,0 +1,22 @@
+/*=====================================================
+  Declared as literal object (All variables are static)	  
+  =====================================================*/
+var Starvrun = {
+	SERVER_NAME : "localhost",	// server name of Starvrun game
+	PORT : 4344,				// port of Starvrun game
+
+	FRAME_RATE : 30,			// frame rate of Starvrun game
+
+	GRID_SIZE : 32,				// a grid length in pixels
+
+	RIGHT : 0,
+	UP: 1,
+	LEFT: 2,
+	DOWN: 3,
+
+	EMPTY: -1,
+	WALL: 0,
+	FREE: 1,
+	PELLET: 2,
+	POWERUP: 3
+}
