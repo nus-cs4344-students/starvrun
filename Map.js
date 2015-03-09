23436a5aee68d579c38245e332e3c94b021e23a0
Edrick Rudy Putra <edrick@pigeonlab.com>
2015-03-09 16:19:29 +0800

    --remove: remove all pixel functions since a new decision is made to use GRID instead of pixels in game logic

--- Map.js
+++ Map.js
@@ -14,36 +14,16 @@ function Map() {
 		return height*Starvrun.GRID_SIZE;
 	}
 
-	//input: either x or y in pixels
-	//return the x or y in grid
-	this.pxToGrid = function (pos) {
-		return Math.floor(pos/Starvrun.GRID_SIZE);
-	}
-
 	//input: either x or y in grid
 	//return the x or y in pixels
 	this.gridToPx = function (pos){
-		return pos*Starvrun.GRID_SIZE;
+		return (pos+0.50)*Starvrun.GRID_SIZE;
 	}
 	
-
-	//input: x and y in pixels
-	//return the content of the map in pixel coordinate x,y
-	this.getMapContentPx = function (x,y) {
-		return this.getMapContent(this.pxToGrid(x), this.pxToGrid(y));
-	}
-
 	//input: x and y in grid
 	//return the content of the map in grid coordinate x,y
 	this.getMapContent = function (x,y) {
 		return grids[x][y];
-	}
-
-	//input: x and y in pixels
-	//modify the content of the map in pixel coordinate x,y
-	//return none
-	this.setMapContentPx = function (x,y) {
-		this.setMapContent(this.pxToGrid(x), this.pxToGrid(y));
 	}
 
 	//input: x and y in grid
@@ -52,7 +32,6 @@ function Map() {
 	this.setMapContent = function (x,y,i) {
 		grids[x][y] = i;
 	}
-
 
 	//input: x and y in grid
 	//try to eat a pellet in a grid coordinate, if a pellet/powerup is found, set it to free block
@@ -71,12 +50,6 @@ function Map() {
 	//set the grid to free block
 	this.eatAt = function(x,y) {
 		grids[x][y] = Starvrun.FREE;
-	}
-
-	//input: x and y in pixels
-	//return true if the map in pixel coordinate x and y is passable, return false otherwise
-	this.canPassPx = function(x,y) {
-		return this.canPass(this.pxToGrid(x), this.pxToGrid(y));
 	}
 
 	//input: x and y in grid
