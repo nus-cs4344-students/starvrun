2f849d2884f0d4cf176c4d7974e1db239996d938
yanxian <duyanxian1990@hotmail.com>
2015-03-09 14:32:06 +0800

    Added Global Object For Constants

---
+++ Starvrun.js
@@ -0,0 +1,13 @@
+/*=====================================================
+  Declared as literal object (All variables are static)	  
+  =====================================================*/
+var Starvrun = {
+        // For Future Use
+	PORT : 4344,				// port of Pong game
+	SERVER_NAME : "localhost"               // server name of Pong game
+	//SERVER_NAME : "172.28.181.216"	// server name of Pong game
+};
+
+// For node.js require
+global.Starvrun = Starvrun;
+
